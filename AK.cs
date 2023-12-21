84a4ce46f2fec724f07118a962141e352d203866
Avi Kumar <149712920+frogsjumps@users.noreply.github.com>
2023-12-21 11:49:04 -0800

    Add files via upload

---
+++ AK.cs
@@ -0,0 +1,21 @@
+﻿Random numberGen = new Random();
+
+int rollNumber1 = 0;
+int rollNumber2 = 1;
+int attempts = 0;
+
+Console.WriteLine("Press enter to roll the dice.\n");
+
+while(rollNumber1 != rollNumber2) {
+    Console.ReadKey();
+
+    rollNumber1 = numberGen.Next(1, 7);
+    rollNumber2 = numberGen.Next(1, 7);
+
+    Console.WriteLine("You rolled: " + rollNumber1);
+    Console.WriteLine("You rolled: " + rollNumber2 +"\n");
+    attempts++;
+}
+Console.WriteLine("You got matching rolls in " + attempts + " attempts");
+
+Console.ReadLine();
